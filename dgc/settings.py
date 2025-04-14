ee80ed4612bbd20ff500e248ee1193f70af7b655
ssshiponu <sshiponuddin22@gmail.com>
2025-04-14 18:47:36 +0600

    host added

--- dgc/settings.py
+++ dgc/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-@u7w8r*scy_-#tu%0gslak*06uw2fqaunyo4j83uevme^%+c)w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['.onrender.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
